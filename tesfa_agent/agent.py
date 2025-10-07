a3d0b12c51b46967129b96a56f1d293028fa63bc
birhanu <birhanutbelay@gmail.com>
2025-10-07 18:28:58 +0300

    updated agent.py

--- tesfa_agent/agent.py
+++ tesfa_agent/agent.py
@@ -1,15 +1,18 @@
 from google.adk.agents import LlmAgent
 import os
 from .tools import retrieve_context, predict_health_risk
+from dotenv import load_dotenv
 from .prompt import instruction_text
 
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
 health_agent = LlmAgent(
     model="gemini-2.5-flash",
-    name="TesfaAIAgent",
+    name="tesfa_agent",
     description="Predicts long-term health risks in post-conflict regions using RAG and local BioGPT.",
     instruction=instruction_text,
    tools=[retrieve_context, predict_health_risk]
